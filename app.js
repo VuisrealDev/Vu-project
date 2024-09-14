0ece49261b8cef6c7e7c185385d61d105f20c053
VuisrealDev <lethiminhphuong1977cr@gmail.com>
2024-09-14 20:02:34 +0700

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const exp = require('constants')
 const express = require('express')
+const { platform } = require('os')
+const { sourceMapsEnabled } = require('process')
 const app = express()
 const serv = require('http').Server(app)
 
@@ -9,9 +11,15 @@ app.get('/', function(req, res) {
 
 app.use(express.static(__dirname))
 
+const SOCKET_LIST = {}
+
 const io = require('socket.io')(serv, {})
 
 io.sockets.on('connection', function(socket) {
+    socket.id = Math.random()
+
+    SOCKET_LIST[socket.id] = socket
+
     socket.on('new player', data => {
         io.emit('new player', data)
     })
@@ -23,11 +31,14 @@ io.sockets.on('connection', function(socket) {
         io.emit('sendMessage', data)
     })
 
-    
+    socket.on('disconnect', function() {
+        io.emit('playerDis')
+        delete SOCKET_LIST[socket.id]
+    })
 
-    
 })
 
 serv.listen(2000, function() {
     console.log('Server started at port*: 2000')
 })
+
