3949fa9239a652a5da777215bbcaf8b2d86f6cfd
VuisrealDev <lethiminhphuong1977cr@gmail.com>
2024-09-26 18:48:00 +0700

    server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const socketIO = require("socket.io");
 // classes
 const Game = require("./game/Game");
 
-
 const FRAME_TIME = Math.floor(1000 / 60);
 
 var app = express();
@@ -21,6 +20,15 @@ app.use("/build", express.static(__dirname + "/build"));
 // Routing
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+io.on("connection", function (socket) {
+  socket.on("userConnected", function (data) {
+    io.emit("userConnected", data);
+  });
+  socket.on('sendChat', function(data) {
+    io.emit('sendChat', data)
+  })
 });
 
 // GAME CLOCK
